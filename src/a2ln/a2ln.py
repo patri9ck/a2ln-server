8db15522e467375bf1ca64a7c2cdca55a8d49a4a
Patrick Zwick <git@patri9ck.dev>
2026-06-13 02:30:04 +0200

    Removed shebang and added copyright header

--- src/a2ln/a2ln.py
+++ src/a2ln/a2ln.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-#  Copyright (C) 2022  Patrick Zwick and contributors
+#  Copyright (C) 2023  Patrick Zwick and contributors
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
